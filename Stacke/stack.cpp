69cdd6ef0a598aa5dce9bdd184841644ba3d2581
OsmanXwiinder <osmansaifi30@gmail.com>
2025-04-18 11:38:30 +0530

    git bug fix

--- Stacke/stack.cpp
+++ Stacke/stack.cpp
@@ -7,9 +7,20 @@ using namespace std;
 
 class Stack{
     vector<int> stack;
-}
+
+    void push(int val){
+        stack.push_back(val);
+    }
+
+    void pop(){
+        stack.pop_back();
+    }
+
+    
+
+};  
 
 int main(){
-    
+
     return 0;
 }
