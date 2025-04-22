e97466d9f3e1954497ec72cd538bebeb7b203e6d
OsmanXwiinder <osmansaifi30@gmail.com>
2025-04-22 18:34:02 +0530

    new

--- Stacke/stack.cpp
+++ Stacke/stack.cpp
@@ -6,21 +6,48 @@ using namespace std;
 
 
 class Stack{
+    public:
     vector<int> stack;
 
-    void push(int val){
+    // push for stack 
+    void push(int val){   // 0(1)
         stack.push_back(val);
-    }
+    }   
 
+    // pop for stack
     void pop(){
+        if(isEmpty()){
+            cout << "stack is empty !" << endl;
+            return;
+        }
         stack.pop_back();
     }
 
-    
+    int top(){
+        if(isEmpty()){
+            cout << "stack is Empty !" << endl;
+            return -1;
+        }
+         int lastIdx = stack.size() -1;
+         return stack[lastIdx];
+    }
+
+    bool isEmpty(){
+       return stack.size() == 0;
+    }
 
 };  
 
 int main(){
+    Stack s;
+    s.push(3);
+    s.push(2);
+    s.push(1);
+    while (!s.isEmpty())
+    {
+        cout << s.top() << " " ;
+        s.pop();
+    }
 
     return 0;
 }
---
+++ Stacke/stack1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+template<class T>  // we need use a type of T : T is any Data type
+
+class tack{
+    public:
+    vector<T>stak;
+
+    void push(T val){
+        stak.push_back(val);
+    }
+
+    void pop(){
+        if(isEmpty()){
+            cout << "Stack Is Empty Nothing is Exist" << endl;
+        }
+        stak.pop_back();
+    }
+
+    T top(){
+        if(isEmpty()){
+            cout << "Stack Is Empty Nothing is Exist" << endl;
+        }
+        int lastIdx = stak.size() -1;
+        return lastIdx
+    }
+
+    void isEmpty(){
+        if(stak.size() -1 == 0){
+            cout << "Stack Is Empty Nothing is Exist" << endl;
+        }
+    }
+
+
+};
+
+
+int main(){
+    tack<int>s;
+
+    s.push(3);
+    s.push(2);
+    s.push(1);
+    while (!s.isEmpty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
+    
+
+    return 0;
+}
