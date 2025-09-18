c1091752faff3e63f8a94f9328dfcf0404edea65
OsmanXwiinder <osmansaifi30@gmail.com>
2025-09-18 16:31:41 +0530

    doing:

---
+++ BST/build-BST.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+// node building
+class Node {
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->data = val;
+        left  = NULL;
+        right  = NULL;
+    }
+
+};
+
+// insertion logic or function
+Node* insert(Node* root, int val){  // o(logn)
+    if (root == NULL) // base cases
+    {
+      root =  new Node(val);
+      return root;
+    } 
+
+    if (val < root->data)  // left subtree
+    {
+        root->left  = insert(root->left,val);
+    } else  // right subtree
+    {
+        root->right = insert(root->right,val);
+    }
+    
+    return root;
+}
+
+// build BST
+Node* buildBST(int arr[], int n){
+    Node* root  = NULL;
+
+    for (int i = 0; i < n; i++)
+    {
+        root = insert(root, arr[i]);
+    }
+
+    return root;
+}
+// inorder
+void inorder(Node* root){
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}   
+
+// search key in BST
+bool searchKey(Node* root, int key){     // 0 (h)
+    if (root == NULL){ return false;} // base case
+    if (root->data == key){ return true;} // found base case
+
+    if (root->data > key  ){
+        return searchKey(root->left,key); // left subtree
+    } else {
+        return searchKey(root->right,key);} // right subtree
+}
+
+int main(){
+    // int arr[6] = {5,1,3,4,2,7};
+    int arr2[9] = {8,5,3,1,4,6,10,11,14};
+    int n = sizeof(arr2)/sizeof(int);
+    Node* root = buildBST(arr2,n);
+    inorder(root);
+    cout << boolalpha << endl;
+    cout << "key exists ?: "<< searchKey(root,4) << endl;
+    return 0;
+}
---
+++ BST/intro.cpp
@@ -0,0 +1,5 @@
+//-- inorder Traversal of BST gives a sorted sequence
+//-- left-subtree always < root node
+//-- right - subtree always > root node
+//-- every node of BST → unique
+
