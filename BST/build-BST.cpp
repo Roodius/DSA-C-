c6d63c469427ef33f27291f413d55ec11bd22784
OsmanXwiinder <osmansaifi30@gmail.com>
2025-09-21 16:09:59 +0530

    >

--- BST/build-BST.cpp
+++ BST/build-BST.cpp
@@ -68,15 +68,80 @@ bool searchKey(Node* root, int key){     // 0 (h)
         return searchKey(root->right,key);} // right subtree
 }
 
+// Inorder Sucessor
+Node* getInorderSuccessor(Node* root){
+    while (root->left == NULL)
+    {
+        root =  root->left;
+    }
+    return root;
+    
+}
+
 // deletion
+Node* deleteNode(Node* root, int val){
+    if (root == NULL)
+    {
+        return root;
+    } else if (val < root->data){
+        root->left = deleteNode(root->left,val);
+    } else if (val > root->data){
+        root->right = deleteNode(root->right,val);
+    } else {  // 3 cases childer
+        // root == val
+        //case1 :0 children
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            root = NULL;
+            return root;
+        }
+        //case2 : one child
+        if (root->left == NULL || root->right == NULL)
+        {
+            return root->left == NULL ? root->right : root->left;
+        }
+
+        // case3 : two child (successor, predessor)
+        Node* successor = getInorderSuccessor(root->right);
+        root->data = successor->data;
+        root->right = deleteNode(root->right,successor->data);
+        
+        return root;        
+    }
+    return NULL ;
+}
+
+// Print in Range Start = 5 , end = 12 ;
+void PrintInRange(Node* root, int start, int end){
+    if (root == NULL){
+        return ;
+    }
+
+    if (root->data >= start && root->data <= end)
+    {
+        cout << root->data << " ";
+        PrintInRange(root->left,start,end);
+        PrintInRange(root->right,start,end);
+
+    } else if (root->data < start){   // search only in right sub tree
+        PrintInRange(root->right, start,end);
+
+    } else if (root->data > end)  // search only in left sub tree
+    { 
+        PrintInRange(root->left, start,end);
+    }
+}
+
 
 int main(){
     // int arr[6] = {5,1,3,4,2,7};
     int arr2[9] = {8,5,3,1,4,6,10,11,14};
     int n = sizeof(arr2)/sizeof(int);
     Node* root = buildBST(arr2,n);
-    inorder(root);
-    cout << boolalpha << endl;
-    cout << "key exists ?: "<< searchKey(root,4) << endl;
+    
+
+    PrintInRange(root,5,12);
+    cout << endl;
     return 0;
 }
